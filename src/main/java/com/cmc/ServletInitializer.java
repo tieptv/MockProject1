d1af83363ea2cc654a0432e3dac43b08b08ec8ed
tieptv <tiepnguyenvantv97@gmail.com>
2018-11-08 16:08:13 +0700

    first commit

---
+++ src/main/java/com/cmc/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.cmc;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MockProject1Application.class);
+	}
+
+}
